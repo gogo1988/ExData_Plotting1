96cbed7cb33a723b10bb831b4897dcbfe3e33624
gogo1988 <gogo848@sina.com>
2015-12-13 20:20:25 +0100

    refined assignment 1

--- plot4.R
+++ plot4.R
@@ -14,6 +14,8 @@ require(dplyr)
 data<-fread("household_power_consumption.txt",header=TRUE,sep=";",stringsAsFactors = FALSE,na.strings = "?")
 
 dataTwoDays<-filter(data,grepl("(^1/2/2007)|(^2/2/2007)",Date))
+dataTwoDaysCleaned<-mutate(dataTwoDays, Date=dataDate<-as.Date(dataTwoDays$Date,"%d/%m/%Y"))
+dateTime <- strptime(paste(dataTwoDaysCleaned$Date, dataTwoDaysCleaned$Time, sep=" "), "%Y-%m-%d %H:%M:%S")
 
 png(filename = "plot4.png", width = 480, height = 480, units = "px")
 par(mfcol=c(2,2))
